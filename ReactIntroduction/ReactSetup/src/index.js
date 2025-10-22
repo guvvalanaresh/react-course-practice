a9a7e19855b380676e941729d6d7d328fc031cdb
guvvalanaresh <gnareshkumarreddy7@gmail.com>
2025-10-22 21:27:50 +0530

    Setting up a fresh react project using npm and Learned how webpackages is setup and used

---
+++ ReactIntroduction/ReactSetup/src/index.js
@@ -0,0 +1,20 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+const heading = React.createElement("h1", {id : "title"}, "Welcome to React");
+
+const app = document.getElementById("root");
+
+const root = ReactDOM.createRoot(app);
+
+root.render(heading);
+
+//If you use the above code in the html file by importing it wont work because we should bundle all our code into single javascript file
+
+
+//React + ReactDOM + Our code ==> bundle.js
+
+//npx ==> node package execute --> it will get the us the dependencies and modules into our project from some remote area makes our project ready-made to start our project
+
+//Below is the command for downloading the webpack using npx
+//npx webpack
