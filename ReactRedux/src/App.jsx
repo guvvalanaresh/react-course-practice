3587dba8f82bc07ae2b501441cd91c5730cfe5e3
guvvalanaresh <gnareshkumarreddy7@gmail.com>
2025-11-11 21:30:16 +0530

    Learned React Redux Setup how it passes the to maintain the continuity between components

---
+++ ReactRedux/src/App.jsx
@@ -0,0 +1,80 @@
+import { Provider, useDispatch, useSelector } from "react-redux"
+import store, { ageSelector, countSelector } from "./store"
+// useDispatch
+// useSelector
+import './App.css'
+
+const Age = () => {
+  console.log("Age component rendered");
+  const dispatch = useDispatch();
+
+  const incrementAge = () => {
+    dispatch({ type: "inc_age" });
+  }
+
+  const decrementAge = () => {
+    dispatch({ type: "dec_age" });
+  }
+
+  return (
+    <div className="box">
+        <h3>Age component</h3>
+        <button className="border border-black bg-gray-400 rounded px-2 mr-2 cursor-pointer" onClick={incrementAge}>Increment Age</button>
+        <button className="border border-black bg-gray-400 rounded px-2 mr-2 cursor-pointer" onClick={decrementAge}>Decrement Age</button>
+    </div>
+  );
+}
+
+const Counter = () => {
+  console.log("Counter component rendered");
+  const dispatch = useDispatch();
+
+  const incrementCount = () => {
+    dispatch({ type: "inc_count" });
+  }
+
+  const decrementCount = () => {
+    dispatch({ type: "dec_count" });
+  }
+  return (
+    <div className="box">
+        <h3>Counter component</h3>
+        <button className="border border-black bg-gray-400 rounded px-2 mr-2 cursor-pointer" onClick={incrementCount}>Increment Counter</button>
+        <button className="border border-black bg-gray-400 rounded px-2 mr-2 cursor-pointer" onClick={decrementCount}>Decrement Counter</button>
+    </div>
+  );
+}
+
+const View = () => {
+  console.log("View component rendered");
+  const count = useSelector(countSelector);
+  const age = useSelector(ageSelector);
+
+  return (
+    <div className="box">
+        <p>
+          <b>Age:</b> {age}
+        </p>
+         <p>
+          <b>Count:</b> {count}
+        </p>
+    </div>
+  );
+}
+
+const App = () => {
+  return (
+    <div>
+      <Provider store={store}>
+          <h3 className="text-2xl font-semibold">App Component</h3>
+          <div id="container">
+            <Age />
+            <Counter />
+            <View />
+          </div>
+      </Provider>
+    </div>
+  )
+}
+
+export default App
---
+++ ReactRedux/src/store.js
@@ -0,0 +1,23 @@
+import { legacy_createStore as createStore } from 'redux';
+
+const rootReducer = (state = { age:24, count: 0 }, action) => {
+    if (action.type === "inc_age") return { ...state, age: state.age + 1 };
+    if (action.type === "dec_age") return { ...state, age: state.age - 1 };
+    if (action.type === "inc_count") return { ...state, count: state.count + 1 };
+    if (action.type === "dec_count") return { ...state, count: state.count - 1 };
+    return state;
+};
+
+const store = createStore(rootReducer);
+
+export default store;
+
+export const countSelector = (state) => {
+    // Here it gets the actual current state
+    console.log(state);
+    return state.count;
+}
+
+export const ageSelector = (state) => {
+    return state.age;
+}
